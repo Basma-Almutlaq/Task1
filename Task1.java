842de8f2207c740f1297cb41d08549d3ead5f35a
Basma-Almutlaq <basmaalmutlaq04@gmail.com>
2025-06-17 14:51:06 +0300

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,12 +1,1 @@
-import java.util.Scanner;
-
-public class Task1 {
-    private static Scanner in;
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-
-    }
-
-    
+ 
