efd1c9f7da15ce994bba3831ca4030f98bbc7e12
Basma-Almutlaq <basmaalmutlaq04@gmail.com>
2025-06-17 14:48:55 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task1 {
+    private static Scanner in;
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+    }
+
+    
