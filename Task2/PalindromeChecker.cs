8e4dec4352bf47ad06c9c9e16be80ddce50e4ac8
Basma-Almutlaq <basmaalmutlaq04@gmail.com>
2025-06-19 17:33:36 +0300

    Create program class

--- Task2/PalindromeChecker.cs
+++ Task2/PalindromeChecker.cs
@@ -23,12 +23,5 @@ public class PalindromeChecker
 
         return true;
     }
-
-    public static void Main()
-    {
-        Console.WriteLine(IsPalindrome("Racecar"));           // True
-        Console.WriteLine(IsPalindrome("A man, a plan, a canal, Panama")); // True
-        Console.WriteLine(IsPalindrome("Hello"));             // False
-    }
     
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+ static void Main()
+    {
+        Console.WriteLine(IsPalindrome("Racecar"));           // True
+        Console.WriteLine(IsPalindrome("A man, a plan, a canal, Panama")); // True
+        Console.WriteLine(IsPalindrome("Hello"));             // False
+    }
+}
